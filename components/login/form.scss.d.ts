19c647c4c221d678f73694b351b935ca76d43c86
benka-me <newcratie@gmail.com>
2020-04-17 11:02:52 +0200

    rm cached files

--- components/login/form.scss.d.ts
+++
@@ -1,7 +0,0 @@
-export const main: string;
-export const title: string;
-export const projectBoard: string;
-export const footer: string;
-export const error: string;
-export const inputFeedback: string;
-export const outline: string;
